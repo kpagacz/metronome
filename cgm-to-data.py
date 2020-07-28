e2a075035704876567011be4183acfd80ad758f9
Konrad Pagacz <konrad.pagacz@gmail.com>
2020-07-28 11:04:27 +0200

    0.0.2

--- cgm-to-data.py
+++ cgm-to-data.py
@@ -1,28 +1,46 @@
 import csv
 import numpy as np 
 
-from config import RECORDS, MAX_ADDITIONAL_RECORDS, INTERVAL, file_name, RECORD_LENGTH
+from config import RECORDS_TRAIN, RECORDS_TEST, MAX_ADDITIONAL_RECORDS, INTERVAL, RECORD_LENGTH
 
-with open(file_name, "r", newline="") as source:
-    reader = csv.reader(source)
-    last_row = next(reader)
-    with open("data.csv", "w", newline="") as destination:
-        writer = csv.writer(destination)
-        batch = []
-        for i in range(RECORD_LENGTH):
-            batch.append(next(reader))
+def time_differences(arr):
+    for i, time in enumerate(arr[1:]):
+        print(i, time)
+
+def window_approach_feature_engineering(file_name_source, file_name_destination):
+    with open(file_name_source, "r", newline="") as source:
+        reader = csv.reader(source)
+        next(reader)
+        with open(file_name_destination, "w", newline="") as destination:
+            writer = csv.writer(destination)
+            batch = []
+            for _ in range(RECORD_LENGTH):
+                batch.append(next(reader))
 
 
-        for reader_row in reader:
-            batch_last_row = batch[0]
-            differences = []
-            for row in batch[1:]:
-                differences.append(float(row[0]) - float(batch_last_row[0]))
-                batch_last_row = row
+            for reader_row in reader:
+                batch_last_row = batch[0]
+                differences = []
+                for row in batch[1:]:
+                    differences.append(float(row[0]) - float(batch_last_row[0]))
+                    batch_last_row = row
 
-            labels = [row[1] for row in batch]
-            writer.writerow([*differences, *labels])
+                labels = [row[1] for row in batch]
+                writer.writerow([*differences, *labels])
 
-            batch.append(reader_row)
-            batch = batch[1:]
+                batch.append(reader_row)
+                batch = batch[1:]
+
+def lstm_approach(file_name_source, file_name_destination):
+    with open(file_name_source, "r", newline="") as source:
+        reader = csv.reader(source)
+        next(reader)
+        with open(file_name_destination, "w", newline="") as destination:
+            pass
+
+
+window_approach_feature_engineering("cgm-train.csv", "window-data-train.csv")
+window_approach_feature_engineering("cgm-eval.csv", "window-data-eval.csv")
+
+
 
---
+++ model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf 
+import csv 
+import numpy as np 
+
+
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+
+
+
