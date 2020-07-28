147651e6175193d5f700bc10f4abb9dd9787b3f0
Konrad Pagacz <konrad.pagacz@gmail.com>
2020-07-28 12:51:30 +0200

    0.0.3

--- 1-cgm-to-data.py
+++ 1-cgm-to-data.py
@@ -36,11 +36,41 @@ def lstm_approach(file_name_source, file_name_destination):
         reader = csv.reader(source)
         next(reader)
         with open(file_name_destination, "w", newline="") as destination:
-            pass
+            writer = csv.writer(destination)
+            batch = []
+            for _ in range(RECORD_LENGTH):
+                batch.append(next(reader))
+
+            times = list(zip(*batch))
+            times_np = np.array([float(value) for value in times[0]])
+
+            differences = np.diff(times_np)
+
+            y_label = times[1][-1]
+
+            writer.writerow([*differences, y_label])
+            
+            for row in reader:
+                batch.append(row)
+                batch = batch[1:]
+                times = list(zip(*batch))
+                times_np = np.array([float(value) for value in times[0]])
+
+                differences = np.diff(times_np)
+
+                y_label = times[1][-1]
+
+                writer.writerow([*differences, y_label])
 
 
-window_approach_feature_engineering("cgm-train.csv", "window-data-train.csv")
-window_approach_feature_engineering("cgm-eval.csv", "window-data-eval.csv")
+            
 
 
+# Window approach
+window_approach_feature_engineering("cgm-train.csv", "window-data-train-15.csv")
+window_approach_feature_engineering("cgm-test.csv", "window-data-eval-15.csv")
+
+# LSTM approach
+lstm_approach("cgm-train.csv", "lstm-data-train-15-variables.csv")
+lstm_approach("cgm-test.csv", "lstm-data-eval-15-variables.csv")
 
